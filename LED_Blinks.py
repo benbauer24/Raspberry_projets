e035448f3f12defb2ae0d0f48b90788bb476a6b6
Benbauer24 <nouoaben@yahoo.fr>
2021-06-03 06:33:28 -0400

    Adding the led code blinking .py file

--- LED_Blinks.py
+++ LED_Blinks.py
@@ -1,1 +1,45 @@
-print("This is for testing")
+"""
+This code file performs LED blinks using RPi 4
+
+"""
+
+
+import RPi.GPIO as GPIO
+import time
+
+led_pin = 17
+
+
+def setup():                                      #Function to set GPIO numbering mode and pin configuration
+    GPIO.setmode(GPIO.BCM)                        # Setting the GPIO numbering mode to BCM
+    GPIO.setup(led_pin, GPIO.OUT)                 # LED pin set to output mode
+    GPIO.output(led_pin, GPIO.LOW)                # Let Pin initially power off
+    print(f"LED set on GPIO ID: {led_pin}")
+
+
+def blink():                                      # Function to perform LED blinking in a looping way
+    while True:
+        GPIO.output(led_pin, GPIO.HIGH)        
+        time.sleep(1)
+        print("The LED is on >>>")
+        GPIO.output(led_pin, GPIO.LOW)
+        time.sleep(1)
+        print("The LED is off <<<")
+
+def clear():                                      # Fiunction to clear and cleanup the code at the end
+    GPIO.output(led_pin, GPIO.LOW)
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+
+    print("Led blinking code is running ")
+
+    setup()
+
+    try:
+        blink()
+    
+    except KeyboardInterrupt:
+        clear()
+
